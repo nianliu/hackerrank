bae0af58659aea0ff15d1239fb99bc456b876bb7
Nian Liu <nliu@bol.com>
2024-11-22 00:12:35 +0100

    init

---
+++ src/main/kotlin/io/github/nianliu/KotlinMain.kt
@@ -0,0 +1,12 @@
+package io.github.nianliu
+
+class KotlinMain {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello World!")
+        }
+    }
+
+}
